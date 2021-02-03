203d08a70be78ba21338b5aeb9937ce599015d10
appelquist <n.appelquist@live.se>
2021-02-03 18:18:02 +0100

    Add Circle class

---
+++ lab-2-csd/Circle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_2_csd
+{
+    public class Circle : Shape
+    {
+        int typePoints = 2;
+        public Circle(int xCoordinate, int yCoordinate, int circumference, int instancePoints) : base(xCoordinate, yCoordinate, circumference, instancePoints)
+        {
+        }
+    }
+}
