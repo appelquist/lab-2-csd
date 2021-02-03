d9ffeba6b1ef781eac7d61b6dc0a1cfce6926c08
appelquist <n.appelquist@live.se>
2021-02-03 18:19:06 +0100

    Add Point class

---
+++ lab-2-csd/Point.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_2_csd
+{
+    public class Point
+    {
+        int xCoordinate;
+        int yCoordinate;
+
+        public Point(int xCoordinate, int yCoordinate)
+        {
+            this.xCoordinate = xCoordinate;
+            this.yCoordinate = yCoordinate;
+        }
+    }
+}
