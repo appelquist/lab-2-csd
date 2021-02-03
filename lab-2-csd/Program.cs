d27fdd62240e03c7a96fc40f8dafc91da9bb82d0
appelquist <n.appelquist@live.se>
2021-02-03 07:34:42 +0100

    initial commit

---
+++ lab-2-csd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab_2_csd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
