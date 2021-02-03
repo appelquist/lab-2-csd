9dc72693eba90b93b7f4c632a908dea0233e79c4
appelquist <n.appelquist@live.se>
2021-02-03 18:18:34 +0100

    Add Square class

---
+++ lab-2-csd/Square.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_2_csd
+{
+    public class Square : Shape
+    {
+        int typePoints = 2;
+        public Square(int xCoordinate, int yCoordinate, int circumference, int instancePoints) : base(xCoordinate, yCoordinate, circumference, instancePoints)
+        {
+        }
+    }
+}
