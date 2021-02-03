cae0b76f9355e449651aca3871218425fa48bcf9
appelquist <n.appelquist@live.se>
2021-02-03 18:18:23 +0100

    Add Shape class

---
+++ lab-2-csd/Shape.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_2_csd
+{
+    public class Shape
+    {
+        int xCoordinate;
+        int yCoordinate;
+        int circumference;
+        int instancePoints;
+        int typePoints;
+
+        public Shape(int xCoordinate, int yCoordinate, int circumference, int instancePoints)
+        {
+            this.xCoordinate = xCoordinate;
+            this.yCoordinate = yCoordinate;
+            this.circumference = circumference;
+            this.instancePoints = instancePoints;
+        }
+    }
+}
