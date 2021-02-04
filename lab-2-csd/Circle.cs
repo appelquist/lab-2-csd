c7711c6610dd062b1550ce7ca05a4ffcfbdedcf8
appelquist <n.appelquist@live.se>
2021-02-04 18:55:44 +0100

    Added Area and isPointInside function

--- lab-2-csd/Circle.cs
+++ lab-2-csd/Circle.cs
@@ -6,9 +6,38 @@ namespace lab_2_csd
 {
     public class Circle : Shape
     {
-        int typePoints = 2;
-        public Circle(int xCoordinate, int yCoordinate, int circumference, int instancePoints) : base(xCoordinate, yCoordinate, circumference, instancePoints)
+        public Circle(int xCoordinate, int yCoordinate, int circumference, int instancePoints)
         {
+            this.xCoordinate = xCoordinate;
+            this.yCoordinate = yCoordinate;
+            this.circumference = circumference;
+            this.instancePoints = instancePoints;
+            this.typePoints = 2;
+        }
+
+        public override double Area()
+        {
+            double area = (Math.Pow(this.circumference, 2)) / (Math.PI * 4);
+            return area;
+        }
+
+        public override bool isPointInside(Point point)
+        {
+            double radius = this.circumference / (2 * Math.PI);
+
+            int circleX = this.xCoordinate;
+            int circleY = this.yCoordinate;
+            int pointX = point.xCoordinate;
+            int pointY = point.yCoordinate;
+
+            double distance = Math.Sqrt(Math.Pow(pointX - circleX, 2) + Math.Pow(pointY - circleY, 2));
+
+            if (distance < radius)
+            {
+                return true;
+            }
+            else return false;
+
         }
     }
 }
