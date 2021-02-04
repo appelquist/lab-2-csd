d1f560921e6c7fb13a32cbaaf43d77059fa3b410
appelquist <n.appelquist@live.se>
2021-02-04 18:57:48 +0100

    Added score function

--- lab-2-csd/Point.cs
+++ lab-2-csd/Point.cs
@@ -6,13 +6,35 @@ namespace lab_2_csd
 {
     public class Point
     {
-        int xCoordinate;
-        int yCoordinate;
+        public int xCoordinate { get; set; }
+        public int yCoordinate { get; set; }
 
         public Point(int xCoordinate, int yCoordinate)
         {
             this.xCoordinate = xCoordinate;
             this.yCoordinate = yCoordinate;
+        }
+
+        public int Score(List<Shape> shapes)
+        {
+            double hitScore = 0;
+            double missScore = 0;
+            Console.WriteLine(this.xCoordinate);
+            Console.WriteLine(this.yCoordinate);
+
+            foreach (Shape shape in shapes)
+            {
+                if (shape.isPointInside(this))
+                {
+                    hitScore += shape.ShapeScore();
+                }
+                else if (!shape.isPointInside(this))
+                {
+                    missScore += shape.ShapeScore();
+                }
+            }
+
+            return (int)Math.Round(hitScore -  (0.25 * missScore));
         }
     }
 }
