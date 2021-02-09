e52584ad88f3de06f671a6beca5e8aace3ea2d8f
appelquist <n.appelquist@live.se>
2021-02-09 12:37:37 +0100

    added support for cml arguments

--- lab-2-csd/Program.cs
+++ lab-2-csd/Program.cs
@@ -19,49 +19,34 @@ namespace lab_2_csd
             }
 
             List<Shape> shapes = new List<Shape>();
+            Point guess;
             string shapesInput = args[0];
             int xInput = Convert.ToInt32(args[1][0]);
             int yInput = Convert.ToInt32(args[1][1]);
-            Console.WriteLine(xInput);
-            //string[] coordinatesInput = args[1].Split(',');
-            //coordinatesInput[0].Trim('(');
-            //coordinatesInput[1].Trim(')');
+            string[] coordinates = args[1].Split(' ');
+
+            try
+            {
+                guess = new Point(Convert.ToInt32(coordinates[0]), Convert.ToInt32(coordinates[1]));
+            }
+            catch
+            {
+                Console.WriteLine("Point in wrong format.");
+                Console.WriteLine("Usage: dotnet run <shapes, (x-coordinate(int), y-coordinate(int))>");
+                return 1;
+            }
 
             if (!AddShapes(shapesInput))
             {
                 Console.WriteLine("Please provide shapes in csv format");
-                Console.WriteLine("Usage: dotnet run <\"SHAPE,X,Y,LENGTH,POINTS;CIRCLE,3,1,13,100;\"");
+                Console.WriteLine("Usage: dotnet run <\"SHAPE,X,Y,LENGTH,POINTS;CIRCLE,3,1,13,100;\", (x-coordinate(int), y-coordinate(int)");
                 return 1;
             }
 
-            if (!(int.TryParse(args[1][0], out int output) && int.TryParse(args[1][1], out _)))
-            {
-                Console.WriteLine("Second argument shoul be of type integer");
-                Console.WriteLine("Usage: dotnet run <file.csv (x-coordinate(int), y-coordinate(int))>");
-                return 1;
-            }
 
-            int score;
+            Console.WriteLine("Your score: " + guess.Score(shapes));
+
             return 0;
-
-            ////Ask for x coordinate and store in variable
-            //Console.WriteLine("Enter a x coordinate: ");
-            //int xCoordinate = Convert.ToInt32(Console.ReadLine());
-
-            ////Ask for y coordinate and store in variable
-            //Console.WriteLine("Enter a y coordinate: ");
-            //int yCoordinate = Convert.ToInt32(Console.ReadLine());
-
-            ////User specified point
-            //Point point = new Point(xCoordinate, yCoordinate);
-
-            ////Ask for shapes in csv format and store in variable
-            //Console.WriteLine("Enter shapes: (in CSV format: SHAPE, X, Y, LENGTH, POINTS)");
-            //string csvShapes = Console.ReadLine();
-
-            //AddShapes(csvShapes);
-            //Console.WriteLine("Your score: " + point.Score(shapes));
-
 
             bool AddShapes(string csvShapes)
             {
